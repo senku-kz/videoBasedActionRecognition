46b64c12e76485efd2d829247dead3148cef29de
senku-kz <senku.kz@gmail.com>
2023-11-27 03:55:06 +0600

    re train

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ from model import LstmNeuralNetwork
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    ds = ActionDataset()
-    ds.read_folders_list()
-    ds.read_files_list()
-    ds.gen_dataset_np()
-    ds.process_data()
+    # ds = ActionDataset()
+    # ds.read_folders_list()
+    # ds.read_files_list()
+    # ds.gen_dataset_np()
+    # ds.process_data()
 
     nn = LstmNeuralNetwork()
-    nn.train_the_model()
-    nn.model_summary()
-    nn.model_save()
+    # nn.train_the_model()
+    # nn.model_summary()
+    # nn.model_save()
+
     nn.model_load()
     nn.model_evaluation()
 
